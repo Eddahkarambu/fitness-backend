441bacddf22fb0381835f3d083725193b8f0ae3c
Eddahkarambu <gikundaeddah@gmail.com>
2022-09-29 16:01:27 +0300

    hash  password using bcrypt

--- App.js
+++ App.js
@@ -3,23 +3,20 @@ const bodyParser = require("body-parser");
 const express = require ("express");
 const app = express();
 const mongoose =require ("mongoose");
-require('dotenv/config');
-
-app.use(bodyParser.json());
-
-// import routes
 const usersRoute = require ('./routes/users');
+require('dotenv/config');
+app.use(bodyParser.json());
 app.use('/users', usersRoute);
 
 // routes
-app.get('/', (req,res) => {
+app.get('/', async(req,res) => {
     res.send('we are on home');
 });
 
-// listen
 
 // Connect to db
 mongoose.connect(process.env.DB_CONNECTION, () =>console.log('connected to db!'));
 
+// listenp
 app.listen(3001);
 
